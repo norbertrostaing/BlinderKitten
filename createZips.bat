4744f8bc3821fcb7211db1286dba7a028ad7d62b
Norbert Rostaing <rostaing.norbert@gmail.com>
2022-04-12 23:10:26 +0200

    add zip file to download

---
+++ createZips.bat
@@ -0,0 +1,4 @@
+"C:\Program Files\7-Zip\7z.exe" d "BlinderKitten.zip" *
+"C:\Program Files\7-Zip\7z.exe" a -tzip "BlinderKitten.zip" "./Binaries/App/BlinderKitten.exe"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "BlinderKitten.zip" "./Binaries/App/Servus.dll"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "BlinderKitten.zip" "./Binaries/App/fixtures.tar.gz"
