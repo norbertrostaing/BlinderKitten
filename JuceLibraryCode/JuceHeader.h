6a4b8b8090c354f122ffb51e49a9f1a042b20def
Norbert rostaing <rostaing.norbert@gmail.Com>
2023-02-07 14:02:04 +0100

    Installer beta

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -46,7 +46,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "BlinderKitten";
     const char* const  companyName    = "";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "1.0.1b1";
+    const int          versionNumber  = 0x10001;
 }
 #endif
