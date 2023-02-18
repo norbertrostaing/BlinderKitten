82d9515803b98781f14e539f3135781f1bf67178
Norbert rostaing <rostaing.norbert@gmail.Com>
2023-02-18 15:40:59 +0100

    1.0.1b3

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -52,7 +52,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "BlinderKitten";
     const char* const  companyName    = "Norbert Rostaing";
-    const char* const  versionString  = "1.0.1b1";
+    const char* const  versionString  = "1.0.1b3";
     const int          versionNumber  = 0x10001;
 }
 #endif
