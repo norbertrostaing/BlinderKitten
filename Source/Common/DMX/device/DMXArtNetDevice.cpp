416841fafa2021b812fbb0682b7563f854477def
Norbert rostaing <rostaing.norbert@gmail.Com>
2023-03-02 19:06:04 +0100

    DMX In finalized

--- Source/Common/DMX/device/DMXArtNetDevice.cpp
+++ Source/Common/DMX/device/DMXArtNetDevice.cpp
@@ -222,10 +222,11 @@ void ArtnetSocket::run()
 				if (opcode == DMX_OPCODE)
 				{
 					//int sequence = artnetPacket[12];
-					int universe = artnetPacket[14] & 0xF;
-					int subnet = (artnetPacket[14] >> 4) & 0xF;
-					int net = artnetPacket[15] & 0x7F;
+					int universe = receiveBuffer[14] & 0xF;
+					int subnet = (receiveBuffer[14] >> 4) & 0xF;
+					int net = receiveBuffer[15] & 0x7F;
 
+					LOG("universe " << universe);
 					for (auto& i : InterfaceManager::getInstance()->items)
 					{
 						DMXInterface* inter = dynamic_cast<DMXInterface*>(i);
