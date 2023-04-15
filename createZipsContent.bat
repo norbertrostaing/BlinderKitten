f9804e5af7b16640d0008f18e02a56b649a028e2
Norbert rostaing <rostaing.norbert@gmail.Com>
2023-04-15 12:35:34 +0200

    Shared data update

--- createZipsContent.bat
+++ createZipsContent.bat
@@ -1,2 +1,2 @@
 "C:\Program Files\7-Zip\7z.exe" d "BlinderKittenShared.zip" *
-"C:\Program Files\7-Zip\7z.exe" a -tzip "BlinderKittenShared.zip" "C:\repos\BlinderKitten\Binaries\App\SharedData\*"
+"C:\Program Files\7-Zip\7z.exe" a -tzip "BlinderKittenShared.zip" "SharedData\*"
