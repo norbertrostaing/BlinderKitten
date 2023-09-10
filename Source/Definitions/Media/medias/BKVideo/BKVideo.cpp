c2893b3cedf6921ec6b18e6f56c810150044e19a
Norbert rostaing <rostaing.norbert@gmail.Com>
2023-09-10 16:33:41 +0400

    WIP VLC

--- Source/Definitions/Media/medias/BKVideo/BKVideo.cpp
+++ Source/Definitions/Media/medias/BKVideo/BKVideo.cpp
@@ -9,15 +9,23 @@
 */
 
 #include "BKVideo.h"
+#include "vlcpp/vlc.hpp"
 
 BKVideo::BKVideo(var params) :
-	Media(params)
+	Media(params),
+    Thread("BKVideoFileMedia")
 {
 	filePath = addFileParameter("File path", "File path", "");
+    const char* argv[1] = { "-vvv" };
+    VLCInstance = libvlc_new(1, argv);
 }
 
 BKVideo::~BKVideo()
 {
+    if (VLCMediaPlayer != nullptr) {
+        libvlc_media_player_release(VLCMediaPlayer);
+    }
+    libvlc_release(VLCInstance);
 }
 
 void BKVideo::clearItem()
@@ -32,5 +40,107 @@ Colour BKVideo::getColourAtCoord(Point<float>* point)
 
 void BKVideo::onContainerParameterChanged(Parameter* p)
 {
+
+    if (p == filePath) {
+        String f = filePath->getFile().getFullPathName();
+        VLCMedia = libvlc_media_new_path(VLCInstance, f.toRawUTF8());
+        //VLCMedia = libvlc_media_new_location(VLCInstance, f.toRawUTF8());
+        VLCMediaPlayer = libvlc_media_player_new_from_media(VLCMedia);
+    
+        // libvlc_video_set_callbacks(); ---- https://cpp.hotexamples.com/fr/examples/-/-/libvlc_video_set_format_callbacks/cpp-libvlc_video_set_format_callbacks-function-examples.html
+        //libvlc_video_lock_cb;
+        libvlc_video_cleanup_cb;
+
+        libvlc_video_set_callbacks(VLCMediaPlayer, NULL, unlock, display, this);
+        libvlc_video_set_format_callbacks(VLCMediaPlayer, setupVideo, cleanVideo);
+        //libvlc_video_set_callbacks(VLCMediaPlayer, lock, unlock, display, this);
+        libvlc_media_release(VLCMedia);
+        play();
+    }
+
+#if 0
+    /* This is a non working code that show how to hooks into a window,
+     * if we have a window around */
+    libvlc_media_player_set_xwindow(mp, xid);
+    /* or on windows */
+    libvlc_media_player_set_hwnd(mp, hwnd);
+    /* or on mac os */
+    libvlc_media_player_set_nsobject(mp, view);
+#endif
+
+    /* play the media_player */
+
+    //sleep(10); /* Let it play a bit */
+
+    /* Stop playing */
+
+}
+
+void BKVideo::play()
+{
+    if (VLCMediaPlayer != nullptr) {
+        libvlc_media_player_play(VLCMediaPlayer);
+    }
+}
+
+void BKVideo::stop()
+{
+    if (VLCMediaPlayer != nullptr) {
+        libvlc_media_player_stop(VLCMediaPlayer);
+    }
+}
+
+void BKVideo::run()
+{
+}
+
+void BKVideo::threadLoop()
+{
+}
+
+
+
+void* BKVideo::lock(void* opaque, void** planes)
+{
+    //LOG("oui");
+    /*VideoSource* _this = static_cast<VideoSource*>(data);
+
+    *pixelData = _this->pixelData;
+
+    EnterCriticalSection(&_this->textureLock);
+    */
+    return 0;
+}
+
+void BKVideo::unlock(void* data, void* id, void* const* pixelData)
+{
+    LOG("oui");
+    /*
+    VideoSource* _this = static_cast<VideoSource*>(data);
+
+    if (_this->isInScene) {
+        _this->GetTexture()->SetImage(*pixelData, GS_IMAGEFORMAT_BGRA, _this->GetTexture()->Width() * 4);
+    }
+
+    LeaveCriticalSection(&_this->textureLock);
+
+    assert(id == NULL); // picture identifier, not needed here 
+    */
+}
+
+void BKVideo::display(void* opaque, void* picture)
+{
+    LOG("oui");
+}
+
+unsigned BKVideo::setupVideo(void** opaque, char* chroma, unsigned* width, unsigned* height, unsigned* pitches, unsigned* lines)
+{
+    LOG("ok");
+    return 1;
+}
+
+void BKVideo::cleanVideo(void* opaque)
+{
+    LOG("meh");
 }
 
--- Source/Definitions/Media/medias/BKVideo/BKVideo.h
+++ Source/Definitions/Media/medias/BKVideo/BKVideo.h
@@ -14,9 +14,13 @@
 #include "Definitions/Media/Media.h"
 
 class FixturePatch;
+struct libvlc_instance_t;
+struct libvlc_media_player_t;
+struct libvlc_media_t;
 
 class BKVideo :
-    public Media
+    public Media,
+    public Thread
 {
 public:
     BKVideo(var params = var());
@@ -28,8 +32,25 @@ public:
     Colour getColourAtCoord(Point<float>* point) override;
     void onContainerParameterChanged(Parameter* p) override;
     
-    String getTypeString() const override { return "Video"; }
-    static BKVideo* create(var params) { return new BKVideo(); };
+    String getTypeString() const override { return "VideoFile"; }
+    static BKVideo* create(var params) { return new BKVideo(); }
+
+    libvlc_instance_t* VLCInstance;
+    libvlc_media_player_t* VLCMediaPlayer;
+    libvlc_media_t* VLCMedia;
+
+    void play(); 
+    void stop();
+
+    void run() override;
+    void threadLoop();
+
+    static void* lock(void* opaque, void** planes);
+    static void unlock(void* data, void* id, void* const* pixelData);
+    static void display(void* data, void* id);
+
+    static unsigned setupVideo(void** opaque, char* chroma, unsigned* width, unsigned* height, unsigned* pitches, unsigned* lines);
+    static void cleanVideo(void* opaque);
 
     //virtual MediaUI* createUI() {return new BKVideo(); };
 };
