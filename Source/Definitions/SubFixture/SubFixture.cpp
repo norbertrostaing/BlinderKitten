85c374a772291b48562f85aeef1b818cd8f5b49e
Norb <rostaing.norbert@gmail.com>
2024-02-14 22:47:57 +0100

    crash fix (thx Edrig)

--- Source/Definitions/SubFixture/SubFixture.cpp
+++ Source/Definitions/SubFixture/SubFixture.cpp
@@ -12,6 +12,7 @@
 #include "SubFixture.h"
 #include "../../Brain.h"
 #include "Fixture/Fixture.h"
+#include "UserInputManager.h"
 
 SubFixture::SubFixture() :
 	channelsMap()
@@ -27,6 +28,11 @@ SubFixture::~SubFixture()
 {
 	channelsMap.clear();
 	channelsContainer.clear();
+	if (UserInputManager::getInstance()->currentProgrammer != nullptr) {
+		if (UserInputManager::getInstance()->currentProgrammer->currentUserCommand != nullptr) {
+			UserInputManager::getInstance()->currentProgrammer->currentUserCommand->computeValues();
+		}
+	}
 }
 
 int SubFixture::MyHashGenerator::generateHash(SubFixture* key, int upperLimit) const
