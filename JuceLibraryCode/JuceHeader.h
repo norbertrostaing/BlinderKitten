9cc8ad8e1b0627707afb8f1b424a0cd04fd9e456
Norb <rostaing.norbert@gmail.com>
2024-12-07 01:55:39 +0100

    1.0.1b73

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -52,7 +52,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "BlinderKitten";
     const char* const  companyName    = "Norbert Rostaing";
-    const char* const  versionString  = "1.0.1b72";
+    const char* const  versionString  = "1.0.1b73";
     const int          versionNumber  = 0x10001;
 }
 #endif
