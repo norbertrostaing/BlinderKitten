4bb6701b75dc9838ef9f42b67dd58c14dfbc538a
Norb <rostaing.norbert@gmail.com>
2025-04-30 23:03:17 +0200

    cuelist load add lock

--- Source/UI/CuelistLoadWindow.cpp
+++ Source/UI/CuelistLoadWindow.cpp
@@ -45,6 +45,8 @@ void CuelistLoadWindow::resized()
 
     if (cueIds.size() == 0) {return;}
 
+    const juce::ScopedLock lock(buttonsLock);
+
     float prevId = cueIds[0];
     for (int i = 0; i < buttons.size(); i++) {
         bool isNewLine = false;
@@ -61,30 +63,32 @@ void CuelistLoadWindow::resized()
         }
         TextButton* b = buttons[i];
         b->setWantsKeyboardFocus(false);
-        b->setBounds(x*w, y*h, w, h);
+        b->setBounds(x * w, y * h, w, h);
         b->addListener(this);
         prevId = id;
     }
-    btnContainer.setSize(getWidth()-10, (y + 1) * h);
+    
+    btnContainer.setSize(getWidth() - 10, (y + 1) * h);
 
 }
 
 void CuelistLoadWindow::fillButtons(Cuelist* c) {
     currentTarget = c;
-    // bug ici ! quand load une cuelist, rajouter un lock ?
+    const juce::ScopedLock lock(buttonsLock);
     buttons.clear();
     cueIds.clear();
     for (int i = 0; i < c->cues.items.size(); i++) {
-        TextButton *temp = new TextButton();
+        TextButton* temp = new TextButton();
         temp->setButtonText(c->cues.items[i]->niceName);
         if (c->cueA == c->cues.items[i]) {
             temp->setColour(TextButton::buttonColourId, Colour(128, 128, 128));
-            temp->setColour(TextButton::textColourOffId, Colour(255,255,255));
+            temp->setColour(TextButton::textColourOffId, Colour(255, 255, 255));
         }
         btnContainer.addAndMakeVisible(temp);
         buttons.add(temp);
         cueIds.add(c->cues.items[i]->id->floatValue());
     }
+    
 }
 
 void CuelistLoadWindow::showWindow()
@@ -101,7 +105,7 @@ void CuelistLoadWindow::showWindow()
         addAndMakeVisible(viewport);
         viewport.setViewedComponent(&btnContainer);
         viewport.setBounds(0, 0, w, h);
-        btnContainer.setSize(w-10, 200);
+        btnContainer.setSize(w - 10, 200);
 
         closeWindow();
         fillButtons(currentCuelist);
@@ -116,14 +120,15 @@ void CuelistLoadWindow::showWindow()
 
 
 void CuelistLoadWindow::loadCuelist(Cuelist* c, bool triggerGoWhenSelected)
-{   
+{
     triggerGo = triggerGoWhenSelected;
     currentCuelist = c;
-    MessageManager::callAsync([this](){showWindow();});
+    MessageManager::callAsync([this]() {showWindow(); });
 }
 
 void CuelistLoadWindow::buttonClicked(Button* b)
 {
+    const juce::ScopedLock lock(buttonsLock);
     int index = buttons.indexOf((TextButton*)b);
     if (currentTarget != nullptr && currentTarget->cues.items.size()>index) {
         currentTarget->nextCueId->setValue(currentTarget->cues.items[index]->id->floatValue());
@@ -136,6 +141,7 @@ void CuelistLoadWindow::buttonClicked(Button* b)
     posX = test.getX();
     posY = test.getY();
     closeWindow();
+    
 }
 
 void CuelistLoadWindow::closeWindow() {
--- Source/UI/CuelistLoadWindow.h
+++ Source/UI/CuelistLoadWindow.h
@@ -50,6 +50,8 @@ public:
     int posX = 0;
     int posY = 0;
 
+    juce::CriticalSection buttonsLock;
+
 private:
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CuelistLoadWindow)
 };
