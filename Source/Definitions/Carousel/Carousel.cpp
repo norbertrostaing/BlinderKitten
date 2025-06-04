aabbebb8653c4577e32a6e552c417c591700906b
Norb <rostaing.norbert@gmail.com>
2025-06-04 13:23:47 +0200

    carousel lock bug fix

--- Source/Definitions/Carousel/Carousel.cpp
+++ Source/Definitions/Carousel/Carousel.cpp
@@ -100,7 +100,7 @@ Carousel::~Carousel()
 			rows.items[i]->paramContainer.items[j]->parentCarousel = nullptr;
 		}
 	}
-	isComputing.enter();
+	isComputing.exit();
 }
 
 void Carousel::onControllableFeedbackUpdateInternal(ControllableContainer* cc, Controllable* c) {
